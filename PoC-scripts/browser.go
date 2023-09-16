93a9f4671e6229a64f4138af8126b6798dcd70a5
nu11secur1ty <venvaropt@gmail.com>
2023-09-16 06:51:45 +0300

    Add files via upload

---
+++ PoC-scripts/browser.go
@@ -0,0 +1,38 @@
+//nu11secur1ty
+package main
+
+import (
+    "fmt"
+    "log"
+    "os"
+    "os/exec"
+    "runtime"
+)
+
+func main() {
+    baseURL := "https://www.nu11secur1ty.com/"
+    if len(os.Args) == 2 {
+        baseURL = os.Args[1]
+    }
+    openBrowser(baseURL)
+}
+func openBrowser(targetURL string) {
+    var err error
+
+    switch runtime.GOOS {
+    case "linux":
+        err = exec.Command("xdg-open", targetURL).Start()
+        // TODO: "Windows Subsytem for Linux" is also recognized as "linux", but then we need
+        // err = exec.Command("rundll32.exe", "url.dll,FileProtocolHandler", targetURL).Start()
+    case "windows":
+        err = exec.Command("rundll32.exe", "url.dll,FileProtocolHandler", targetURL).Start()
+    case "darwin":
+        err = exec.Command("open", targetURL).Start()
+    default:
+        err = fmt.Errorf("unsupported platform %v", runtime.GOOS)
+    }
+    if err != nil {
+        log.Fatal(err)
+    }
+
+}
