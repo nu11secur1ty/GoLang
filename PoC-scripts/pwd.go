c9f9bc7aabe40431f5c05a5d746115668eddd1c0
nu11secur1ty <venvaropt@gmail.com>
2023-09-16 07:23:51 +0300

    Add files via upload

---
+++ PoC-scripts/pwd.go
@@ -0,0 +1,16 @@
+//nu11secur1ty
+package main
+
+import (
+    "fmt"
+    "os"
+)
+
+func main() {
+    pwd, err := os.Getwd()
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+    fmt.Println(pwd)
+}
